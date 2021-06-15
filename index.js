78e6537e5cec8ff6a6b26f9567f0bd1e7f11f689
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-15 16:57:30 -0700

    test the value of the variable

--- index.js
+++ index.js
@@ -7,29 +7,39 @@
 arr1 = [7, 8, 9, 10, 11, 12];
 
 // split the array in half
-const half = Math.ceil(arr1.length/2);
+ let half = arr1.length/2;
 
-firstHalf = arr1.slice(0, half);
-//let arrFirstHalf = [];
-secondHalf = arr1.slice(-half + 1);
+// firstHalf = arr1.slice(0, half + 1);
+// //let arrFirstHalf = [];
+// secondHalf = arr1.slice(-half + 1);
 
 
-// compare the two halves.  If one is larger, return the element at the last index in the array.  
-//If both are the same size, return the element at the last index in the array in section 1, and 
-//the first element at the 0 index of the second half.
+// // compare the two halves.  If one is larger, return the element at the last index in the array.  
+// //If both are the same size, return the element at the last index in the array in section 1, and 
+// //the first element at the 0 index of the second half.
 
-function getMiddle(array) {
+// function getMiddle(array) {
     
-    if (array[firstHalf.length] > array[Math.abs(secondHalf.length)]) {
-        console.log(`${array[firstHalf.length - 1]}`);
-    } else { 
-        console.log(`${array[firstHalf.length]}, ${secondHalf[0]}`);
-        console.log(`this is wild: ${secondHalf[0]}`);
+//     if (array[firstHalf.length] > array[Math.abs(secondHalf.length)]) {
+//         console.log(`${array[firstHalf.length - 1]}`);
+//     } else { 
+//         console.log(`${array[firstHalf.length]}, ${secondHalf[0]}`);
+//         console.log(`this is wild: ${secondHalf[0]}`);
+//     }
+// }
+
+// console.log(half);
+// console.log(`firstHalf: ${firstHalf.length}`);
+// console.log(`secondHalf: ${secondHalf.length}`);
+
+// getMiddle(arr1);
+
+function testFunction() {
+    if (half % 2 === 0) {
+    console.log("it's even");
+    } else {
+    console.log("it's odd");
     }
 }
 
-console.log(half);
-console.log(`firstHalf: ${firstHalf.length}`);
-console.log(`secondHalf: ${secondHalf.length}`);
-
-getMiddle(arr1);
+testFunction();
